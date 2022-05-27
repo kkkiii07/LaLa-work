1e27ed8315c0209caefc3cbce32ce0cb4de32da9
kkkiii07 <0zm385602rp379z@gmail.com>
2022-05-27 16:27:31 +0900

    rpg を作成

---
+++ rpg/src/world/Matango.java
@@ -0,0 +1,11 @@
+package world;
+
+public class Matango {
+	public int hp;
+	public final int LEVEL = 10;
+	public char suffix; //suffixは、前置詞
+	
+	public void run() {
+		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+	}
+}
---
+++ sukkiri/src/ex5/Ex5_4.java
@@ -0,0 +1,24 @@
+package ex5;
+
+public class Ex5_4 {
+
+	public static void main(String[] args) {
+		double x = calcTriangleArea(10.0, 5.0);
+		System.out.println("三角形の面積は" + x + "平方cmです。");
+		
+		double y = calcCircleArea(5.0);
+		System.out.println("円の面積は" + y + "平方cmです。");
+
+	}
+	
+	public static double calcTriangleArea(double bottom, double height) {
+		double triangle = (bottom * height) / 2;
+		return triangle;
+	}
+	
+	public static double calcCircleArea(double radius) {
+		double circle = (radius * radius) * 3.14;
+		return circle;
+	}
+
+}
